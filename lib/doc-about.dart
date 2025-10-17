cf3090f2e13659f6091a25ddd0ac1bcf9928c615
Zhafrnn <haydarzhafran3@gmail.com>
2025-10-17 09:15:05 +0700

    Belajar flutter

---
+++ lib/doc-about.dart
@@ -0,0 +1,52 @@
+/** PENJELASAN */
+
+/**
+ * STRUKTUR FLUTTER
+ * 
+ * Terdiri dari yang pertama yaitu ada 
+ * - .dart_tool
+ * - .idea
+ * - android
+ *  => Untuk konfigurasi android serta
+ *     library android (native)
+ * - build
+ *  => hasil compile digital
+ * - ios
+ *  => Untuk konfigurasi ios serta
+ * library ios (native)
+ * - lib
+ *  => tempat file dart berada yang nantinya
+ * serung digunakan
+ * - linux
+ *  => untuk konfigurasi linux
+ * - macos
+ *  => untuk konfigurasi macos
+ * - test
+ *  => tempat file untuk testing aplikasi
+ * - windows
+ *  => untuk konfigurasi windows
+ * - pubspec.ysml
+ *  => tempat konfigurasi aplikasi
+ *  => menambahkan library
+ *  => menambahkan asset (gambar, font, dll)
+ * 
+ * Di terminal perintah Flutter pub get
+ * => untuk mengsyncronkan library yang ada di pubcpes.yaml
+ * 
+ * kegunaan perintah Flutter pub upgrade
+ * => mengupgrade library yang ada di pubcpec.yaml
+ * 
+ * kegunaan perintah Flutter run
+ * => menjalankan aplikasi
+ * 
+ * kegunaan perintah Fluter clean
+ * => membersihkan file build
+ * 
+ * library dari flutter bisa dilihat di pub.dev
+ * intl, http, button, dll
+ * 
+ * didalam dart ada 2 statement
+ * - stateless => widget yang tidak memiliki state (static)
+ * - statefull => widget yang memiliki state (dynamic)
+ * 
+ */
